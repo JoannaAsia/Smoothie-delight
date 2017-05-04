45fcdffd41a608c4f9305e113c745a2122f944c7
JoannaAsia <joanna.wojcik91@gmail.com>
2017-05-04 18:54:48 +0200

    gulpfile modification and last touches

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 
-gulp.task('css', function() {
-	return gulp.src('src/css/*.css')
+gulp.task('dist', function() {
+	gulp.src('src/css/*.css')
 	.pipe(postcss([
 		require('autoprefixer')({}),
 		]))
-	.pipe(gulp.dest('dist'));
+	.pipe(gulp.dest('dist/src/css'));
 });
